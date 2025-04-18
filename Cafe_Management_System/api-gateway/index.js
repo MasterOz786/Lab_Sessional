d8a2a63e05931d1ee85598c3abb0d1f223dce9cd
MasterOz786 <i222434@nu.edu.pk>
2025-04-18 12:33:31 +0500

    moved .github folder for workflows

--- Cafe_Management_System/api-gateway/index.js
+++ Cafe_Management_System/api-gateway/index.js
@@ -6,11 +6,11 @@ const port = 3000;
 app.use(express.json());
 
 // Proxy routes to respective services
-app.use('/menu', createProxyMiddleware({ target: 'http://menu-service:3001', changeOrigin: true }));
-app.use('/orders', createProxyMiddleware({ target: 'http://order-service:3002', changeOrigin: true }));
-app.use('/payments', createProxyMiddleware({ target: 'http://payment-service:3003', changeOrigin: true }));
-app.use('/inventory', createProxyMiddleware({ target: 'http://inventory-service:3004', changeOrigin: true }));
-app.use('/customers', createProxyMiddleware({ target: 'customer-service:3005', changeOrigin: true }));
+app.use('/menu', createProxyMiddleware({ target: 'http://localhost:3001', changeOrigin: true }));
+app.use('/orders', createProxyMiddleware({ target: 'http://localhost:3002', changeOrigin: true }));
+app.use('/payments', createProxyMiddleware({ target: 'http://localhost:3003', changeOrigin: true }));
+app.use('/inventory', createProxyMiddleware({ target: 'http://localhost:3004', changeOrigin: true }));
+app.use('/customers', createProxyMiddleware({ target: 'http://localhost:3005', changeOrigin: true }));
 
 app.listen(port, () => {
   console.log(`API Gateway running on port ${port}`);
